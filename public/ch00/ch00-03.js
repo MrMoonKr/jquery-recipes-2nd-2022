793adf10c72415869dda5c2464654bf2c61a8116
Mr.Moon <MrMoonKr@gmail.com>
2024-07-20 17:46:30 +0900

    module 형태로 사용하기 위해 몇가지 테스트 ...

---
+++ public/ch00/ch00-03.js
@@ -0,0 +1,9 @@
+//import $ from "jquery";
+//import jQuery from "jquery";
+//import "jquery-old" ;
+import "jquery";
+
+$( document ).ready( () => {
+    alert( "Hello, World!" );
+    $( "h2" ).text( "Hello, jQuery from CDN !!!" );
+} );
---
+++ public/ch00/ch00-04.js
@@ -0,0 +1,8 @@
+//import $ from './jquery.module.js' ;
+//import "https://code.jquery.com/jquery-3.7.1.min.js" ;
+import "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" ;
+
+$( document ).ready( () => {
+    alert( "Hello, World!" );
+    $( "h2" ).text( "Hello, jQuery from CDN !!!" );
+} );
